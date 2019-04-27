4b5b03dce16074ff23a1799b9588a814631310af
Jonas Gierer <jgierer12@gmail.com>
2019-04-27 19:37:41 +0200

    Add basic blog page

---
+++ src/pages/blog.js
@@ -0,0 +1,11 @@
+import * as React from "react";
+
+import { Layout } from "../components/layout";
+import { Header } from "../components/header";
+
+export default () => (
+  <Layout>
+    <Layout.SEO title="Jonas Gierer's Blog" />
+    <Header />
+  </Layout>
+);
