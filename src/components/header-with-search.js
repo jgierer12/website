f4e5bcc7fd59c628e7ecbe5f6c146f83f766b0a9
Jonas Gierer <jgierer12@gmail.com>
2019-04-28 00:46:42 +0200

    Add header with search component

---
+++ src/components/header-with-search.js
@@ -0,0 +1,72 @@
+import * as React from "react";
+import SearchIcon from "boxicons/svg/regular/bx-search.svg";
+import XIcon from "boxicons/svg/regular/bx-x.svg";
+
+import * as colors from "../colors";
+import { useSearchContext } from "./search-context";
+import { Header } from "./header";
+
+const SearchButton = () => {
+  const { isSearch, setIsSearch } = useSearchContext();
+
+  const toggleSearch = React.useCallback(
+    () => setIsSearch(current => !current),
+    []
+  );
+  const Icon = isSearch ? XIcon : SearchIcon;
+
+  return (
+    <button onClick={toggleSearch}>
+      <Icon role="img" fill="currentColor" css={{ display: `block` }} />
+    </button>
+  );
+};
+
+const SearchInput = () => {
+  const { searchQuery, setSearchQuery, setIsSearch } = useSearchContext();
+  const handleSearchChange = React.useCallback(event => {
+    setSearchQuery(event.target.value);
+  }, []);
+
+  React.useEffect(() => {
+    const escKeyListener = event => {
+      if (event.key === `Escape`) {
+        setIsSearch(false);
+      }
+    };
+    document.addEventListener(`keydown`, escKeyListener);
+    return () => {
+      document.removeEventListener(`keydown`, escKeyListener);
+    };
+  }, []);
+
+  return (
+    <input
+      autoFocus
+      type="text"
+      value={searchQuery}
+      onChange={handleSearchChange}
+      css={{
+        borderBottom: `2px solid ${colors.mono.dark}`,
+        flexGrow: `1`,
+        height: `100%`,
+      }}
+    />
+  );
+};
+
+export const HeaderWithSearch = props => {
+  const { isSearch } = useSearchContext();
+  return (
+    <Header {...props}>
+      {isSearch && (
+        <Header.Left>
+          <SearchInput />
+        </Header.Left>
+      )}
+      <Header.Right>
+        <SearchButton />
+      </Header.Right>
+    </Header>
+  );
+};
