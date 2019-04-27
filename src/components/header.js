cd39a2a85b39e0488465aac0d01b7bee588a96ec
Jonas Gierer <jgierer12@gmail.com>
2019-04-28 00:41:10 +0200

    Simplify header component

--- src/components/header.js
+++ src/components/header.js
@@ -1,30 +1,20 @@
 import * as React from "react";
-import SearchIcon from "boxicons/svg/regular/bx-search.svg";
-import XIcon from "boxicons/svg/regular/bx-x.svg";
 import { graphql, useStaticQuery } from "gatsby";
 import Image from "gatsby-image";
+import { withSlots } from "react-puggy";
 
 import { Link } from "./link";
-import * as colors from "../colors";
-import { SearchContext } from "./search-context";
 
-const SearchButton = ({ icon: Icon, ...props }) => (
-  <button {...props}>
-    <Icon role="img" fill="currentColor" css={{ display: `block` }} />
-  </button>
-);
-
-const SearchInput = props => (
-  <input
-    autoFocus
-    type="text"
-    {...props}
+const BlogLink = () => (
+  <Link
+    to="/blog"
     css={{
-      borderBottom: `2px solid ${colors.mono.dark}`,
-      flexGrow: `1`,
-      height: `100%`,
+      textTransform: `uppercase`,
+      letterSpacing: `1px`,
     }}
-  />
+  >
+    Blog
+  </Link>
 );
 
 const Brand = () => {
@@ -57,35 +47,24 @@ const Brand = () => {
   );
 };
 
-export const Header = () => {
-  const { isSearch, setIsSearch } = React.useContext(SearchContext);
-
-  const toggleSearch = React.useCallback(
-    () => setIsSearch(current => !current),
-    []
-  );
-
-  return (
-    <header css={{ padding: `20px` }}>
-      <div
-        css={{
-          margin: `auto`,
-          maxWidth: `800px`,
-          display: `flex`,
-          justifyContent: `space-between`,
-          alignItems: `center`,
-          "& > * + *": {
-            marginLeft: `20px`,
-          },
-          height: `50px`,
-        }}
-      >
-        {isSearch ? <SearchInput /> : <Brand />}
-        <SearchButton
-          onClick={toggleSearch}
-          icon={isSearch ? XIcon : SearchIcon}
-        />
-      </div>
+export const Header = withSlots(
+  [`Left`, `Right`],
+  ({ slotProps: { Left, Right }, ...props }) => (
+    <header
+      {...props}
+      css={{
+        height: `90px`,
+        padding: `20px`,
+        display: `flex`,
+        justifyContent: `space-between`,
+        alignItems: `center`,
+        "& > * + *": {
+          marginLeft: `20px`,
+        },
+      }}
+    >
+      {Left ? Left.children : <Brand />}
+      {Right ? Right.children : <BlogLink />}
     </header>
-  );
-};
+  )
+);
