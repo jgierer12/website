4ecd142fba214d84376f7ad46f96862b2b15ecef
Jonas Gierer <jgierer12@gmail.com>
2019-04-27 17:10:25 +0200

    Improve post template styles

--- src/templates/post.js
+++ src/templates/post.js
@@ -1,6 +1,11 @@
 import * as React from "react";
 import { graphql } from "gatsby";
 import MDXRenderer from "gatsby-mdx/mdx-renderer";
+import { MDXProvider } from "@mdx-js/react";
+import "typeface-space-mono";
+import "typeface-pt-serif";
+
+import { Layout } from "../components/layout";
 
 export const query = graphql`
   query($id: String) {
@@ -9,6 +14,7 @@ export const query = graphql`
       frontmatter {
         title
       }
+      excerpt
       code {
         body
       }
@@ -18,9 +24,26 @@ export const query = graphql`
 
 export default ({ data: { mdx } }) => {
   return (
-    <div>
-      <h1>{mdx.frontmatter.title}</h1>
-      <MDXRenderer>{mdx.code.body}</MDXRenderer>
-    </div>
+    <Layout>
+      <Layout.SEO description={mdx.excerpt} />
+      <article
+        css={{
+          fontFamily: `PT Serif, serif`,
+          maxWidth: `720px`,
+          margin: `auto`,
+        }}
+      >
+        <h1>{mdx.frontmatter.title}</h1>
+        <MDXProvider
+          components={{
+            inlineCode: props => (
+              <code {...props} css={{ fontFamily: `Space Mono, monospace` }} />
+            ),
+          }}
+        >
+          <MDXRenderer>{mdx.code.body}</MDXRenderer>
+        </MDXProvider>
+      </article>
+    </Layout>
   );
 };
