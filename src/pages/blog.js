a95634887424ffb80f9df11ae8ef8d2504946f29
Jonas Gierer <jgierer12@gmail.com>
2019-04-28 00:48:14 +0200

    Apply preceding changes to blog index page

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,11 +1,29 @@
 import * as React from "react";
+import { Global } from "@emotion/core";
 
 import { Layout } from "../components/layout";
-import { Header } from "../components/header";
+import { AllPostList } from "../components/all-post-list";
+import { BlogLayout } from "../components/blog-layout";
+import { HeaderWithSearch } from "../components/header-with-search";
 
-export default () => (
-  <Layout>
-    <Layout.SEO title="Jonas Gierer's Blog" />
-    <Header />
-  </Layout>
-);
+export default () => {
+  return (
+    <>
+      <Global
+        styles={{
+          body: {
+            // So content doesn't reflow during search
+            // due to appearing or disappearing scrollbar
+            overflowY: `scroll`,
+          },
+        }}
+      />
+      <Layout pageType="blog">
+        <Layout.SEO title="Jonas Gierer's Blog" />
+        <BlogLayout header={HeaderWithSearch}>
+          <AllPostList />
+        </BlogLayout>
+      </Layout>
+    </>
+  );
+};
