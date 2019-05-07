4295ffabbd52e6a62c651c8725383f82d1299b52
Jonas Gierer <jonas@gierer.xyz>
2019-05-07 13:29:52 +0200

    Add icons to social links

--- src/components/post-full/social-links.js
+++ src/components/post-full/social-links.js
@@ -1,10 +1,14 @@
 import * as React from "react";
 import { graphql } from "gatsby";
+import TwitterIcon from "boxicons/svg/logos/bxl-twitter.svg";
+import RedditIcon from "boxicons/svg/logos/bxl-reddit.svg";
+import GithubIcon from "boxicons/svg/logos/bxl-github.svg";
 
 import { Link } from "~/components/link";
 import * as colors from "~/colors";
 import { transition } from "~/transition";
 import { siteMetadata, repo } from "~/config";
+import * as media from "~/media";
 
 export const query = graphql`
   fragment PostFull_SocialLinks on Mdx {
@@ -24,6 +28,89 @@ export const query = graphql`
     }
   }
 `;
+
+const Item = ({ verb = `Discuss`, platform, icon: Icon, url }) => (
+  <li
+    css={{
+      [media.phone.above]: {
+        background: colors.mono.white,
+        padding: `0px 10px`,
+      },
+    }}
+  >
+    <Link
+      to={url}
+      css={{
+        display: `flex`,
+        [media.phone.below]: {
+          flexDirection: `column`,
+        },
+        alignItems: `center`,
+        ...transition(`color`),
+        ...transition.out,
+        ":hover": {
+          ...transition.in,
+          color: colors.mono.medium,
+        },
+      }}
+    >
+      <Icon
+        css={{
+          "&, & g": {
+            // Reddit icon has fill on <g> instead of <svg>
+            fill: `currentColor`,
+          },
+        }}
+        width="1.5em"
+        height="1.5em"
+      />
+      <span
+        css={{
+          [media.phone.below]: {
+            marginTop: `5px`,
+          },
+          [media.phone.above]: {
+            marginLeft: `5px`,
+          },
+        }}
+      >
+        {verb} on {platform}
+      </span>
+    </Link>
+  </li>
+);
+
+const List = ({ children }) => (
+  <ul
+    css={{
+      position: `relative`,
+      padding: `20px`,
+      display: `flex`,
+      fontSize: `90%`,
+      gridRow: `5`,
+      gridColumn: `2`,
+      justifyContent: `space-between`,
+      textAlign: `center`,
+      "& > * + *": {
+        marginLeft: `20px`,
+      },
+      [media.phone.above]: {
+        justifyContent: `center`,
+        "&::before": {
+          content: `''`,
+          position: `absolute`,
+          height: `1px`,
+          background: colors.mono.light,
+          width: `calc(100% - 40px)`,
+          zIndex: `-10`,
+          top: `50%`,
+        },
+      },
+    }}
+  >
+    {children}
+  </ul>
+);
 
 export const SocialLinks = ({ mdx }) => {
   const postUrl = `${siteMetadata.url}${mdx.fields.slug}`;
@@ -47,50 +134,10 @@ export const SocialLinks = ({ mdx }) => {
   );
 
   return (
-    <ul
-      css={{
-        position: `relative`,
-        padding: `20px`,
-        display: `flex`,
-        fontSize: `90%`,
-        gridRow: `5`,
-        gridColumn: `2`,
-        justifyContent: `center`,
-        "&::before": {
-          content: `''`,
-          position: `absolute`,
-          height: `1px`,
-          background: colors.mono.light,
-          width: `calc(100% - 40px)`,
-          zIndex: `-10`,
-          top: `50%`,
-        },
-        li: {
-          background: colors.mono.white,
-          padding: `0px 10px`,
-        },
-        a: {
-          ...transition(`color`),
-          ...transition.out,
-        },
-        "a:hover": {
-          ...transition.in,
-          color: colors.mono.medium,
-        },
-        "& > * + *": {
-          marginLeft: `20px`,
-        },
-      }}
-    >
-      <li>
-        <Link to={twitterUrl}>Discuss on Twitter</Link>
-      </li>
-      <li>
-        <Link to={redditUrl}>Discuss on Reddit</Link>
-      </li>
-      <li>
-        <Link to={githubUrl}>Edit on GitHub</Link>
-      </li>
-    </ul>
+    <List>
+      <Item platform="Twitter" icon={TwitterIcon} url={twitterUrl} />
+      <Item platform="Reddit" icon={RedditIcon} url={redditUrl} />
+      <Item platform="GitHub" verb="Edit" icon={GithubIcon} url={githubUrl} />
+    </List>
   );
 };
