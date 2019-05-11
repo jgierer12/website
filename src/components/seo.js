ff8ee341b5e3c929f58daea45c794e458a02834e
Jonas Gierer <jonas@gierer.xyz>
2019-05-11 21:12:12 +0200

    Use absolute URL for og:image
    Relative URLs are not valid

--- src/components/seo.js
+++ src/components/seo.js
@@ -1,17 +1,32 @@
 import * as React from "react";
 import Helmet from "react-helmet";
+import { graphql, useStaticQuery } from "gatsby";
 
 import { usePage } from "~/contexts/page";
 
 export const SEO = () => {
   const { title, description, image, type } = usePage();
+  const {
+    site: {
+      siteMetadata: { url },
+    },
+  } = useStaticQuery(graphql`
+    query {
+      site {
+        siteMetadata {
+          url
+        }
+      }
+    }
+  `);
+
   return (
     <Helmet>
       <title>{title}</title>
       {description && <meta name="description" content={description} />}
       <meta name="twitter:card" content="summary" />
       <meta property="og:type" content={type || `website`} />
-      {image && <meta property="og:image" content={image} />}
+      {image && <meta property="og:image" content={`${url}${image}`} />}
     </Helmet>
   );
 };
